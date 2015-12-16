dfd6d0ec935de619b43b13130aedc945162b48e3
Brandyn Bold <brandynbold@gmail.com>
2015-12-16 17:28:57 -0500

    Add backend options
    Add the following options to the backend:
    
    - Enabled: turn on/off back to top
    - Persist Method: Choose whether to have the btt button show after scrolling 300px or
    	only when scrolling up.
    - Scroll Speed: Speed in which the page scrolls to the top
    - Text: Text that is displayed for 'back to top'

--- skin/frontend/base/default/js/somethingdigital/back-to-top.js
+++ skin/frontend/base/default/js/somethingdigital/back-to-top.js
@@ -1,12 +1,18 @@
 (function($) {
   var BackToTop = {
     scrollAppears: 300,
-    scrollSpeed: 300,
+    scrollSpeed: BackToTopConfig.speed,
+    persistMethod: BackToTopConfig.persist,
     anchorSide: 'left',
 
     init: function() {
-      this.anchorDiv();
-      this.bindScrollEvent();
+      if(this.persistMethod === 'persist') {
+        this.bindPersistShow();
+      } else if(this.persistMethod === 'scroll') {
+        this.bindScrollShow();
+      } else {
+        return;
+      }
 
       $('#back-to-top').on('click', function(e) {
         e.preventDefault();
@@ -18,7 +24,7 @@
         scrollTop: 0
       }, this.scrollSpeed);
     },
-    bindScrollEvent: function() {
+    bindPersistShow: function() {
       $(window).scroll(function() {
         if ($(window).scrollTop() > 300) {
           $('#back-to-top').fadeIn();
@@ -27,9 +33,18 @@
         }
       });
     },
-    anchorDiv: function() {
-      $('#back-to-top').css({'left': '0'});
-    }
+    bindScrollShow: function() {
+      var lastScrollTop = 0;
+      $(window).scroll(function(e){
+         var st = $(this).scrollTop();
+         if (st > lastScrollTop){
+           $('#back-to-top').fadeOut();
+         } else {
+           $('#back-to-top').fadeIn();
+         }
+         lastScrollTop = st;
+      });
+    },
   }
   BackToTop.init();
 }(jQuery));
